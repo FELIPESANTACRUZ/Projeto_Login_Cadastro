b5bfe6d670dffd78ff61eb805a4e0d5283a9785f
FELIPE SANTA CRUZ <felipesantacruz6@gmail.com>
2020-10-27 13:41:41 -0300

    versionando arquivos e programas do Git para o GitHub

---
+++ Projeto Login e Cadastro/cadastro.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page 2</title>
+    <link rel="stylesheet" href="./CSS/cadastro.css">
+    <link rel="stylesheet" href="./CSS/global.css">
+</head>
+<body>
+    <main>
+
+        <div class="card">
+                <h1>Cadastre-se</h1>
+                <form action="#" method="#">
+                    <input type="text" placeholder="Digite seu nome"/>
+                    <input type="email" placeholder="Digiete seu E-mail"/>
+                    <input type="password" placeholder="Digite a sua senha"/>
+                    <input type="password" placeholder="Confirme sua senha" />
+                    <button>Cadastre-se</button>
+                </form>
+                <a href="./index.php">Já possui cadastro? Realize o login</a>
+        </div>
+
+    </main>
+</body>
+</html>
---
+++ Projeto Login e Cadastro/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/global.css">
+    <link rel="stylesheet" href="./CSS/login.css">
+    <title>Projeto monitoria</title>
+</head>
+
+<body>
+    <main>
+
+
+
+<!-- PHP: Sistema de Login com Níveis de Acesso -->
+
+
+
+        <section class="painel_login">
+            <div class="card">
+                <h1>Entre ou Cadastre-se</h1>
+        
+
+        <form action="#" method="#">
+            
+            <input type="email" placeholder="E-mail" />
+            <input type="password" placeholder="Senha" />
+            <button>Entrar</button>
+
+            <a href="cadastro.php">Ainda nao possui cadastro?</a>
+
+            </div>
+        </form>
+
+        </section>
+        <section class="painel_imagem">
+            <img src="./image/login.svg" alt="login">
+        </section>
+
+
+    </main>
+</body>
+
+</html>
